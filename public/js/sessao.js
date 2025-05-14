49ec3ffa3f8b345bf20e967bb9a1bbf7a669cfd0
andreleao-sys <andre.leao@sptech.school>
2025-05-14 20:18:39 -0300

    Configurando funcionalidade de login integrada ao banco de dados da VM

--- public/js/sessao.js
+++ public/js/sessao.js
@@ -1,11 +1,12 @@
 // sessão
-function validarSessao() {
-    var email = sessionStorage.EMAIL_USUARIO;
-    var nome = sessionStorage.NOME_USUARIO;
 
+function validarSessao() {
+    var fkUsuario = sessionStorage.ID_USUARIO;
+    var nome = sessionStorage.NOME_USUARIO;
+    var email = sessionStorage.EMAIL_USUARIO;
     var b_usuario = document.getElementById("b_usuario");
 
-    if (email != null && nome != null) {
+    if (email != null || nome != null) {
         b_usuario.innerHTML = nome;
     } else {
         window.location = "../login.html";
